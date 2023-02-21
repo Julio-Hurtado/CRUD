e8ad686701aa15c666607a8482bcaaca904725fe
Ernesto Hurtado <calerojulio92@gmail.com>
2023-02-20 22:45:43 -0600

    Se ha modificado la cadena de conexion

--- db/configuraciones/config.go
+++ db/configuraciones/config.go
@@ -1,15 +1,13 @@
 package db
 
+import "fmt"
+
 const (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
 	password = "mrrobot"
-	dbName   = "catalogo_con_go"
+	dbName   = "catalogos_con_go"
 )
 
-const ConnStr = "postgres://postgres:mrrobot@localhost/catalogos_con_go?sslmode=disable"
-
-// var ConnStr = fmt.Sprintf("host=%s port=%d user=%s "+
-// 	"password=%s dbname=%s sslmode=disable",
-// 	host, port, user, password, dbName)
+var ConnStr = fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", user, password, host, dbName)
